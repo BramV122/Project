42841406a49be520b45258ed3d8f0d85312da562
bverstra <bramverstraeten@NOREPLY.com>
2017-05-02 15:28:58 +0200

    VGA controller aangepast

--- Project.v
+++ Project.v
@@ -26,7 +26,7 @@ reg [7:0] red;
 reg [7:0] green;
 reg [7:0] blue;
 
-assign clock = CLOCK_50;
+//assign clock = CLOCK_50;
 assign reset = ~KEY[0];
 
 assign VGA_R = red;
@@ -43,33 +43,36 @@ wire [10:0] display_row;
 wire [15:0] address;
 wire visible;
 
-reg [14:0] pixel = 0;
-assign input_hsync = GPIO[1];
-assign input_vsync = GPIO[3];
+wire [14:0] pixel;
+assign input_hsync = ~GPIO[1];
+assign input_vsync = ~GPIO[3];
 
 reg refresh;
+wire clock;
 
-VGA_Controller controller (.clock(clock), .reset(reset), .display_col(display_col), .display_row(display_row), .visible(visible), .hsync(hsync), .vsync(vsync), .refresh(refresh));
+PLL pll (.inclk0(CLOCK_50), .c0(clock));
 
-//BlockRam blockram (.address(address), .clock(clock), .data(1'b0), .wren(1'b0), .q(pixel));
+VGA_Controller controller (.clock(clock), .reset(reset), .display_col(display_col), .display_row(display_row), .visible(visible), .refresh(refresh), .hsync(input_hsync), .vsync(input_vsync));
 
-wire [2:0] out;
+BlockRam blockram (.address(address), .clock(clock), .data(1'b0), .wren(1'b0), .q(pixel));
+
+reg [2:0] out;
 reg [15:0] write_address;
 reg [2:0] comparator;
 reg wren;
 
-display_ram ram (.rdclock(clock), .wrclock(clock), .data(comparator), .rdaddress(address), .wraddress(write_address), .wren(wren), .q(out));
+//display_ram ram (.rdclock(clock), .wrclock(clock), .data(comparator), .rdaddress(address), .wraddress(write_address), .wren(wren), .q(out));
 
 assign address = {display_col[7:0], display_row[7:0]};
 
-always @(posedge clock or negedge input_hsync or negedge input_vsync) begin
+/*always @(posedge clock or negedge input_hsync or negedge input_vsync) begin
 	refresh = 0;
 	if (!input_hsync) begin
 		if (!input_vsync) begin refresh = 1; end
 	end
-end
+end*/
 
-always @(posedge clock) begin
+/*always @(posedge clock) begin
 	if (display_col < 256 && display_row < 256) begin
 		wren = 1;
 		write_address = address + 1;
@@ -77,34 +80,39 @@ always @(posedge clock) begin
 	end else begin
 		wren = 0;
 	end
-end
-
-always @(posedge clock or posedge reset or posedge refresh) begin
-	if (reset || refresh) begin
-		pixel = 0;
-	end else begin
-		if (visible && (display_col < 256 && display_row < 256)) begin
-			if (out[0]) begin
-				if (pixel[14:10] < 5'b11111) pixel[14:10] = pixel[14:10] + 1;
-			end else begin
-				if (pixel[14:10] > 5'b00000) pixel[14:10] = pixel[14:10] - 1;
-			end
-			if (out[1]) begin
-				if (pixel[9:5] < 5'b11111) pixel[9:5] = pixel[9:5] + 1;
-			end else begin
-				if (pixel[9:5] > 5'b00000)pixel[9:5] = pixel[9:5] - 1;
-			end
-			if (out[2]) begin
-				if (pixel[4:0] < 5'b11111) pixel[4:0] = pixel[4:0] + 1;
-			end else begin
-				if (pixel[4:0] > 5'b00000)pixel[4:0] = pixel[4:0] - 1;
-			end
-		end
-	end
-end
+end*/
 
 always @(posedge clock) begin
-	if (visible && (display_col < 256 && display_row < 256))begin
+	comparator = {GPIO[14], GPIO[4], GPIO[0]};
+	out = comparator;
+end
+
+//always @(posedge clock or posedge reset /*or posedge refresh*/) begin
+//	if (reset /*|| refresh*/) begin
+//		pixel = 0;
+//	end else begin
+//		if (visible /*&& (display_col < 256 && display_row < 256)*/) begin
+//			if (out[0]) begin
+//				if (pixel[14:10] < 5'b11111) pixel[14:10] = pixel[14:10] + 1;
+//			end else begin
+//				if (pixel[14:10] > 5'b00000) pixel[14:10] = pixel[14:10] - 1;
+//			end
+//			if (out[1]) begin
+//				if (pixel[9:5] < 5'b11111) pixel[9:5] = pixel[9:5] + 1;
+//			end else begin
+//				if (pixel[9:5] > 5'b00000)pixel[9:5] = pixel[9:5] - 1;
+//			end
+//			if (out[2]) begin
+//				if (pixel[4:0] < 5'b11111) pixel[4:0] = pixel[4:0] + 1;
+//			end else begin
+//				if (pixel[4:0] > 5'b00000)pixel[4:0] = pixel[4:0] - 1;
+//			end
+//		end
+//	end
+//end
+
+always @(posedge clock) begin
+	if (visible /*&& (display_col < 256 && display_row < 256)*/)begin
 		if (SW[2] == 0) begin red = {pixel[14:10], 3'b000}; end else begin red = 0; end
 		if (SW[1] == 0) begin green = {pixel[9:5], 3'b000}; end else begin green = 0; end
 		if (SW[0] == 0) begin blue = {pixel[4:0], 3'b000}; end else begin blue = 0; end
--- VGA_Controller.v
+++ VGA_Controller.v
@@ -1,27 +1,46 @@
-module VGA_Controller(clock, reset, display_col, display_row, visible, hsync, vsync, refresh);
+module VGA_Controller(clock, reset, display_col, display_row, visible, refresh, hsync, vsync);
 
 //72 Hz 800 x 600 VGA - 50MHz clock 
-  parameter HOR_FIELD    =  799; 
-  parameter HOR_STR_SYNC =  855; 
-  parameter HOR_STP_SYNC =  978; 
-  parameter HOR_TOTAL    = 1042; 
-  parameter VER_FIELD    =  599; 
-  parameter VER_STR_SYNC =  636; 
-  parameter VER_STP_SYNC =  642; 
-  parameter VER_TOTAL    =  665;
+//  parameter HOR_FIELD    =  799; 
+//  parameter HOR_STR_SYNC =  855; 
+//  parameter HOR_STP_SYNC =  978; 
+//  parameter HOR_TOTAL    = 1042; 
+//  parameter VER_FIELD    =  599; 
+//  parameter VER_STR_SYNC =  636; 
+//  parameter VER_STP_SYNC =  642; 
+//  parameter VER_TOTAL    =  665;
+
+	// Horizontal timing
+	parameter HOR_Visible_Area = 800;
+	parameter HOR_Front_porch = 40;
+	parameter HOR_Sync_pulse = 128;
+	parameter HOR_Back_porch = 88;
+	parameter HOR_TOTAL = 1056;
+
+	// Vertical timing
+	parameter VER_Visible_Area = 600;
+	parameter VER_Front_porch = 40;
+	parameter VER_Sync_pulse = 4;
+	parameter VER_Back_porch = 23;
+	parameter VER_TOTAL = 628;
   
   input clock; 
   input reset;                   // reset signal 
   input refresh;						// reset display_col en display_row
   output reg [11:0] display_col; // horizontal counter 
   output reg [10:0] display_row; // vertical counter 
-  output visible;                // signal visible on display 
-  output reg hsync, vsync;
+  output visible;           	   // signal visible on display 
+  //output reg hsync, vsync;
+  input hsync, vsync;
 
-  assign visible = !(display_row > VER_FIELD || display_col > HOR_FIELD);
+  assign visible = !(display_row > (VER_TOTAL - VER_Back_porch - VER_Sync_pulse) || display_col > (HOR_TOTAL - HOR_Back_porch - HOR_Sync_pulse) || display_row < VER_Front_porch || display_col < HOR_Front_porch);
   
-  always @(posedge clock or posedge reset or posedge refresh) begin
-		if (reset || refresh) begin
+  reg previous_hsync;
+  reg previous_vsync;
+
+  
+  always @(posedge clock or posedge reset /*or posedge refresh*/) begin
+		if (reset /*|| refresh*/) begin
 			display_row = 0;
 			display_col = 0;
 		end else begin
@@ -35,10 +54,22 @@ module VGA_Controller(clock, reset, display_col, display_row, visible, hsync, vs
 					display_row = 0;
 				end
 			end
+			/*if (previous_hsync != hsync && hsync == 1) begin
+				display_col = 0;
+				previous_hsync = hsync;
+			end else begin
+				previous_hsync = hsync;
+			end
+			if (previous_vsync != vsync && vsync == 1) begin
+				display_row = 0;
+				previous_vsync = vsync;
+			end else begin
+				previous_vsync = vsync;
+			end*/
 		end
   end
   
-  always @(posedge clock) begin
+  /*always @(posedge clock) begin
 		hsync = 1;
 		if (display_col >= HOR_STR_SYNC && display_col <= HOR_STP_SYNC) begin hsync = 0; end
   end
@@ -46,6 +77,6 @@ module VGA_Controller(clock, reset, display_col, display_row, visible, hsync, vs
   always @(posedge clock) begin
 		vsync = 1;
 		if (display_row >= VER_STR_SYNC && display_row <= VER_STP_SYNC) begin vsync = 0;  end
-	end
+	end*/
 
 endmodule 
